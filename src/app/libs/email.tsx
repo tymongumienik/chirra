8a3ec95c793c20caa811d788fe5a3417f8ac3ff3
Tymon Gumienik <gumienik.tymon@gmail.com>
2025-12-20 19:45:06 +0100

    fix: correct email verification link

--- src/app/libs/email.tsx
+++ src/app/libs/email.tsx
@@ -70,7 +70,7 @@ const VerificationEmail = ({
 export async function sendVerificationEmail(to: string, token: string) {
   // if (!process.env.EMAIL_APP_URL) throw new Error("EMAIL_APP_URL not set");
 
-  const verificationUrl = `${process.env.EMAIL_APP_URL}/verify?token=${token}`;
+  const verificationUrl = `${process.env.EMAIL_APP_URL}/api/verify-email?token=${token}`;
 
   const emailHtml = await render(<VerificationEmail verificationUrl={verificationUrl} />);
 
