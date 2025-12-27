a24b67a95bcfb712d3dea0e459cac83c6bd05002
Tymon Gumienik <gumienik.tymon@gmail.com>
2025-12-27 14:19:36 +0100

    fix: author same as user who sent (firefox)

--- src/app/(chat)/app/components/message-view.tsx
+++ src/app/(chat)/app/components/message-view.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useUserDataStore } from "../scripts/stores/user-data";
 import { Message } from "@/app/api/[[...slugs]]/ws/shared-schema";
+import { useUserStore } from "../scripts/stores/who-am-i";
 
 export function MessageView({
   authorId,
@@ -9,14 +10,17 @@ export function MessageView({
   authorId: string;
   message: (typeof Message)["static"];
 }) {
-  const author = useUserDataStore((s) => s.getUser(authorId));
+  const user = useUserStore((s) => s.user);
+  const authorOther = useUserDataStore((s) => s.getUser(authorId));
+
+  const author = user?.id === authorId ? user : authorOther;
 
   return (
     <div className="flex gap-2 px-4 py-2 hover:bg-gray-900/30 group">
       <div className="shrink-0 mr-1">
         <Image
           src={"/default-avatar.png"}
-          alt={author?.username || ""}
+          alt={author?.displayname || author?.username || ""}
           className="rounded-full"
           width={40}
           height={40}
@@ -27,7 +31,15 @@ export function MessageView({
           <span className="font-semibold text-sm text-white hover:underline cursor-pointer">
             {author?.displayname || author?.username || ""}
           </span>
-          <span className="text-xs text-gray-500">{message.timestamp}</span>
+          <span className="text-xs text-gray-500">
+            {message.createdAt.toLocaleString("en-GB", {
+              day: "numeric",
+              month: "short",
+              year: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </span>
         </div>
         <div className="text-white text-sm leading-relaxed">
           {message.content}
