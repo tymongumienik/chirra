040512c081c287ed982f982312f53cd00f34ebe5
Tymon Gumienik <gumienik.tymon@gmail.com>
2025-12-28 18:31:00 +0100

    feat: frontend for typing users

--- src/app/(chat)/app/components/parts/channel-view.tsx
+++ src/app/(chat)/app/components/parts/channel-view.tsx
@@ -1,5 +1,11 @@
 import { Plus, Smile } from "lucide-react";
-import { useCallback, useLayoutEffect, useRef, useState } from "react";
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import {
   type RequestMessageHistoryData,
   RequestMessageHistoryResponseCompiler,
@@ -14,6 +20,7 @@ import { MessageView } from "../message-view";
 import { ChannelTopBar } from "./channel-top-bar";
 import { Modal } from "../modal";
 import { UserModal } from "../user-modal";
+import { useUserDataStore } from "../../scripts/stores/user-data";
 
 export function ChannelView() {
   const { sendMessageAndWaitForReply } = useWebSocket();
@@ -25,6 +32,7 @@ export function ChannelView() {
   );
   const location =
     view === "dm" ? { user: currentDmUser } : { channel: activeChannel };
+  const getUser = useUserDataStore((s) => s.getUser);
 
   const messages = useMessagesStore((s) => s.getMessages(location)) || [];
   const setMessagesForLocation = useMessagesStore(
@@ -34,6 +42,7 @@ export function ChannelView() {
   const containerRef = useRef<HTMLDivElement>(null);
   const loadingRef = useRef(false);
   const pageRef = useRef(0);
+  const typing = [];
 
   const fetchMessages = useCallback(
     async (pageToFetch: number) => {
@@ -80,13 +89,24 @@ export function ChannelView() {
     });
   }, [fetchMessages]);
 
+  useEffect(() => {
+    console.log("welcome");
+    return () => console.log("goodbye");
+  }, []);
+
   const [openUserModalId, setOpenUserModalId] = useState<string | null>(null);
   const [isUserModalOpen, setIsUserModalOpen] = useState(false);
+  const [atBottom, setAtBottom] = useState(true);
 
   // infinite scroll for older messages
   const handleScroll = () => {
     const container = containerRef.current;
     if (!container || loadingRef.current) return;
+
+    const distanceFromBottom =
+      container.scrollHeight - container.scrollTop - container.clientHeight;
+
+    setAtBottom(distanceFromBottom < 10);
 
     if (container.scrollTop < 100) {
       const nextPage = pageRef.current + 1;
@@ -119,7 +139,7 @@ export function ChannelView() {
         ref={containerRef}
         onScroll={handleScroll}
       >
-        <div className="flex flex-col mt-auto">
+        <div className="flex flex-col mt-auto gap-1 mb-4">
           {messages.map((msg) => (
             <MessageView
               key={msg.id}
@@ -134,7 +154,15 @@ export function ChannelView() {
         </div>
       </div>
 
-      <div className="px-4 pb-3">
+      <div className="px-4 pb-3 relative">
+        <div
+          className={`absolute bottom-0 left-0 w-full h-64 pointer-events-none transition-opacity ${
+            atBottom ? "opacity-0" : "opacity-100"
+          }`}
+          style={{
+            background: "linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,0))",
+          }}
+        />
         <form
           onSubmit={(e) => {
             e.preventDefault();
@@ -144,7 +172,7 @@ export function ChannelView() {
             console.log("Send:", input.value);
             input.value = "";
           }}
-          className="flex items-center gap-4 bg-slate-800 rounded-lg px-4 py-3"
+          className="flex items-center gap-4 bg-slate-800 rounded-lg px-4 py-3 relative"
         >
           <button type="button" className="hover:opacity-80">
             <Plus className="w-6 h-6 text-gray-400" />
@@ -159,6 +187,44 @@ export function ChannelView() {
             <Smile className="w-6 h-6 text-gray-400" />
           </button>
         </form>
+
+        {typing.length > 0 && (
+          <div className="rounded-xl absolute h-4 w-[calc(100%-2rem)] -top-5 flex items-center bg-accent/5">
+            <div className="flex items-center gap-0.5 ml-3">
+              <div className="w-2 h-2 bg-slate-600 rounded-full animate-pulse"></div>
+              <div className="w-2 h-2 bg-slate-600 rounded-full animate-pulse delay-200"></div>
+              <div className="w-2 h-2 bg-slate-600 rounded-full animate-pulse delay-400"></div>
+            </div>
+            <span className="ml-2 text-xs">
+              {typing.length <= 3 && (
+                <>
+                  {typing
+                    .map((x) => getUser(x)?.displayname ?? null)
+                    .filter((x) => x !== null)
+                    .map((name, i, arr) => (
+                      <span key={`typing-${name}`}>
+                        <strong>{name}</strong>
+                        {i < arr.length - 1 ? ", " : ""}
+                      </span>
+                    ))}{" "}
+                  {(() => {
+                    const filtered = typing
+                      .map((x) => getUser(x)?.displayname ?? null)
+                      .filter((x) => x !== null);
+                    return filtered.length === 0
+                      ? "Someone is typing..."
+                      : filtered.length === 1
+                        ? "is typing..."
+                        : filtered.length > 1
+                          ? "are typing..."
+                          : "";
+                  })()}
+                </>
+              )}
+              {typing.length > 3 && "Multiple people are typing..."}
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
